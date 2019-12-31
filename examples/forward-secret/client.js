b4b0888aa1f668cb905a59ce0b81760b7e43429d
Bret Comnes <bcomnes@gmail.com>
2019-12-31 15:51:18 -0700

    Work snapshot

--- examples/forward-secret/client.js
+++ examples/forward-secret/client.js
@@ -1,10 +1,69 @@
 const secureRPC = require('../../.')
 const net = require('net')
 
-const socket = new net.Socket()
+const socket = new net.Socket({
+  allowHalfOpen: true
+})
+
+socket.on('close', () => {
+  console.log('socket closed')
+})
+
+socket.on('connect', () => {
+  console.log('socket connected')
+})
+
+socket.on('drain', () => {
+  console.log('socket drain')
+})
+
+socket.on('end', () => {
+  console.log('socket ended')
+})
+
+socket.on('error', (err) => {
+  console.error('socket errored')
+  console.error(err)
+})
+
+socket.on('lookup', (...args) => {
+  console.log('socket lookup')
+  console.log(args)
+})
+
+socket.on('ready', () => {
+  console.log('socket ready')
+})
+
+socket.on('timeout', () => {
+  console.log('socket timeout')
+})
 
 socket.connect(8124, () => {
   const rpc = secureRPC(socket, true)
+
+  rpc.sec.on('end', () => {
+    console.log('noise-peer end')
+    socket.end()
+  })
+
+  rpc.sec.on('close', () => {
+    console.log('noise-peer close')
+  })
+
+  rpc.sec.on('error', (err) => {
+    console.error('noise-peer errored')
+    console.error(err)
+  })
+
+  rpc.sec.on('timeout', () => {
+    console.error('noise-peer timeout')
+    rpc.sec.end(err => {
+      if (err) console.err(err)
+      console.error('noise-peer ended after timeout')
+    })
+  })
+
   rpc.call('echo', 'hello world', (err, res) => {
     if (err) return console.error(err)
     console.log(res) // [ 'hello world' ]
@@ -24,10 +83,13 @@ socket.connect(8124, () => {
   process.once('SIGTERM', quit)
 
   function quit () {
-    rpc.sec.on('end', () => {
-      console.log('client gracefully shutdown')
-      process.exit(0)
+    rpc.sec.end(null, null, (err) => {
+      debugger
+      if (err) {
+        console.error(err)
+      } else {
+        console.log('client sent `FINISH` message')
+      }
     })
-    rpc.sec.end()
   }
 })
--- examples/forward-secret/server.js
+++ examples/forward-secret/server.js
@@ -1,42 +1,121 @@
 const secureRPC = require('../../.')
 const net = require('net')
+const runp = require('run-parallel')
 
-const server = net.createServer((socket) => {
+const server = net.createServer({
+  allowHalfOpen: true
+}, (socket) => {
   // 'connection' listener.
   console.log('client connected')
+  socket.on('close', () => {
+    console.log('socket closed')
+  })
+
+  socket.on('connect', () => {
+    console.log('socket connected')
+  })
+
+  socket.on('drain', () => {
+    console.log('socket drain')
+  })
+
   socket.on('end', () => {
-    console.log('client disconnected')
+    console.log('socket ended')
+  })
+
+  socket.on('error', (err) => {
+    console.error('socket errored')
+    console.error(err)
+  })
+
+  socket.on('lookup', (...args) => {
+    console.log('socket lookup')
+    console.log(args)
+  })
+
+  socket.on('ready', () => {
+    console.log('socket ready')
+  })
+
+  socket.on('timeout', () => {
+    console.log('socket timeout')
   })
 
   const rpc = secureRPC(socket, false)
 
-  rpc.sec.on('error', err => {
+  rpc.sec.on('end', () => {
+    console.log('noise-peer end')
+    rpc.sec.end(err => {
+      if (err) {
+        console.error('sec error:')
+        console.error(err)
+      }
+      console.log(`session for ${client.id} is ended`)
+    })
+  })
+
+  rpc.sec.on('close', () => {
+    console.log('noise-peer close')
+  })
+
+  rpc.sec.on('error', (err) => {
+    console.error('noise-peer errored')
     console.error(err)
   })
 
+  rpc.sec.on('timeout', () => {
+    console.error('noise-peer timeout')
+    rpc.sec.end(err => {
+      if (err) console.err(err)
+      console.error('noise-peer ended after timeout')
+    })
+  })
+
+  const client = {
+    id: Math.random().toString(36).slice(2)
+  }
+
   rpc.command('echo', (req) => {
+    console.log(req.arguments)
     return req.arguments
   })
 
   rpc.command('ping', (req) => {
-    console.log('ping')
+    console.log(`ping ${client.id}`)
     return 'pong'
   })
+
+  process.once('SIGINT', endSession)
+  process.once('SIGTERM', endSession)
+
+  function endSession () {
+    console.error(`ending session for ${client.id}:`)
+    rpc.sec.end(err => {
+      if (err) {
+        console.error('sec error:')
+        console.error(err)
+      }
+      console.log(`session for ${client.id} is ended`)
+    })
+  }
 })
 
 server.on('error', (err) => {
-  throw err
+  console.error('server error:')
+  console.error(err)
 })
 
 server.listen(8124, () => {
-  console.log('server bound')
+  console.log('server listening on 8124')
 })
 
 process.once('SIGINT', quit)
 process.once('SIGTERM', quit)
 
 function quit () {
-  server.close(() => {
+  console.log('server is shutting down')
+  server.close((err) => {
+    if (err) throw err
     console.log('server gracefully shutdown')
     process.exit(0)
   })
